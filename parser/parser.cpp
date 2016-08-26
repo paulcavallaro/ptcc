398bda4d34f1606d6fa43713f1deff2e3b5f030c
Paul Cavallaro <paulcavallaro@gmail.com>
2016-08-25 23:38:34 -0400

    [Parser] Add simple symbol table

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -3,8 +3,32 @@
 
 namespace ptcc { namespace parser {
 
-int Parser::check_type(const char*) {
-    return IDENTIFIER;
+int Parser::check_type(const char* symbol) {
+    auto entry = m_symbols[insert(symbol)];
+    switch (entry.m_type) {
+    default:
+        return IDENTIFIER;
+    }
+}
+
+ssize_t Parser::find(const char* symbol) {
+    auto res = m_symtable.find(symbol);
+    if (res != m_symtable.end()) {
+        return static_cast<size_t>(res->second);
+    } else {
+        return -1;
+    }
+}
+
+ssize_t Parser::insert(const char* symbol) {
+    auto res = m_symtable.find(symbol);
+    if (res != m_symtable.end()) {
+        return static_cast<size_t>(res->second);
+    } else {
+        auto idx = m_symbols.size();
+        m_symbols.emplace_back(symbol, 0);
+        return idx;
+    }
 }
 
 } }     // ptcc::parser
--- parser/parser.h
+++ parser/parser.h
@@ -1,9 +1,27 @@
 #pragma once
 
+#include <string>
+#include <unordered_map>
+#include <vector>
+
 namespace ptcc { namespace parser {
+
+struct SymTableEntry {
+    SymTableEntry(std::string symbol, int type) :
+      m_symbol(symbol), m_type(type) {}
+
+    std::string m_symbol;
+    int m_type;
+};
 
 struct Parser {
     int check_type(const char* text);
+    ssize_t find(const char* symbol);
+    ssize_t insert(const char* symbol);
+
+ private:
+    std::unordered_map<std::string, size_t> m_symtable;
+    std::vector<SymTableEntry> m_symbols;
 };
 
 } }     // ptcc::parser
