d32be61e159a377a5af194ed8ece5e55adf94687
Paul Cavallaro <paulcavallaro@gmail.com>
2020-04-29 00:14:47 -0400

    [lexer] Outline for token types

---
+++ lexer/token.cc
@@ -0,0 +1,1 @@
+#include "lexer/token.h"
---
+++ lexer/token.h
@@ -0,0 +1,10 @@
+#ifndef __PTCC_LEXER_LEXER_H__
+#define __PTCC_LEXER_LEXER_H__
+
+namespace ptcc {
+
+enum class TokenType {};
+
+}  // namespace ptcc
+
+#endif
